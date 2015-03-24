d58685754da3554422bce4bbd5c9ab11e8c409d5
Elerium <elerium.git@gmail.com>
2015-03-24 20:54:10 +0100

    var -> let

--- src/components/__tests__/app.js
+++ src/components/__tests__/app.js
@@ -3,12 +3,12 @@ jest.autoMockOff()
 import React from 'react/addons'
 import App from '../app.jsx'
 
-var TestUtils = React.addons.TestUtils
+let TestUtils = React.addons.TestUtils
 
 describe('#components.app', () => {
   it('should write "Hello world!" in h1 tag', () => {
-    var app = TestUtils.renderIntoDocument(<App />)
-    var text = TestUtils.findRenderedDOMComponentWithTag(app, 'h1')
+    let app = TestUtils.renderIntoDocument(<App />)
+    let text = TestUtils.findRenderedDOMComponentWithTag(app, 'h1')
     expect(text.props.children).toBe('Hello world!')
   })
 })
