3259b40dc1053c1654bcc3df328b1d5a0bba44ef
Elerium <elerium.git@gmail.com>
2015-04-19 17:37:25 +0200

    semicolons removed

--- preprocessor.js
+++ preprocessor.js
@@ -1,10 +1,12 @@
-var babel = require('babel-core');
+var babel = require('babel')
 
 module.exports = {
   process: function (src, filename) {
-    if(filename.indexOf("node_modules") === -1 && babel.canCompile(filename)) {
-      return babel.transform(src, { filename: filename }).code;
+    if(filename.indexOf('node_modules') === -1 && babel.canCompile(filename)) {
+      return babel.transform(src, {
+        filename: filename
+      }).code
     }
-    return src;
+    return src
   }
-};
+}
